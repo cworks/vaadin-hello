dc2eb868415e5d0291bea3e0690603d3be6fed14
cworks <cmartin@cmartinlt.local>
2015-04-30 22:33:12 -0500

    Added Servlet 3.0 example

--- src/main/java/cworks/vaadin/hello3/HelloUI3.java
+++ src/main/java/cworks/vaadin/hello3/HelloUI3.java
@@ -1,18 +1,27 @@
-package cworks.vaadin.hello;
+package cworks.vaadin.hello3;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import javax.servlet.annotation.WebServlet;
+
 @Title("Hello from my little Vaadin UI")
 @Theme("valo")
-public class HelloUI extends UI {
+public class HelloUI3 extends UI {
 
+    @WebServlet(value = "/helloui3/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = HelloUI3.class)
+    public static class HelloUI3Servlet extends VaadinServlet {
+
+    }
 
     @Override
     protected void init(VaadinRequest request) {
@@ -20,9 +29,9 @@ public class HelloUI extends UI {
         VerticalLayout content = new VerticalLayout();
         setContent(content);
 
-        content.addComponent(new Label("Hello Jack!"));
+        content.addComponent(new Label("Hello Jack this is Servlet 3.0!"));
         content.addComponent(new Button("Press", event -> {
-            Notification.show("Pressing my Button!");
+            Notification.show("Pressing my Servlet 3.0 Button...Look Mom no web.xml!");
         }));
     }
 }
